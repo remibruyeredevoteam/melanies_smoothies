f3942664529e0d0f106fd7729df2ce49dfa1355f
rbruyere <remi.bruyere@devoteam.com>
2026-03-28 11:13:34 +0100

    request df

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ if ingredients_list and len(ingredients_list) == 5 and name:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
